585531af68f26b637282385fdf339c28437d04b5
Beth <bethwambuimuniu@gmail.com>
2017-11-21 22:42:51 +0300

    feat(flask-develop):Add docstring

--- app/tests/test_rsvp.py
+++ app/tests/test_rsvp.py
@@ -6,8 +6,12 @@ from main.person import Person
 
 class TestRsvp(unittest.TestCase):
     """ Rsvp Test class """
-    def setUp(self):
-        self.beth = Rsvp("beth", "bethwambuimuniu@gmail.com",754234567556, 'guest')
 
-    def test_user_inherits_person(self):
-            self.assertTrue(issubclass(Rsvp, Person))
+    def setUp(self):
+        """ setup fixture for testing """
+        self.beth = Rsvp("beth", "bethwambuimuniu@gmail.com", 754234567556,
+                         'guest')
+
+    def test_rsvp_inherits_person(self):
+        """ tests if user is a subclass of person """
+        self.assertTrue(issubclass(Rsvp, Person))
