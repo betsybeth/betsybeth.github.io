bc849ab024171c3efc347a5f4d52d2819aa18b8f
Beth <bethwambuimuniu@gmail.com>
2017-11-21 22:43:36 +0300

    feat(flask-develop):Add docstring

--- app/tests/test_person.py
+++ app/tests/test_person.py
@@ -2,7 +2,10 @@
 import unittest
 from main.person import Person
 
+
 class TestPerson(unittest.TestCase):
     """ Person Test class """
+
     def setUp(self):
+        """ setup fixture for testing """
         self.beth = Person("beth", "bethwambuimuniu@gmail.com" 'guest')
