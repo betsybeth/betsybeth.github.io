838b59e230979a479f49c7635abd05c04dca240f
Beth <bethwambuimuniu@gmail.com>
2017-11-21 18:02:11 +0300

    test(flask-develop): Add test_rsvp.py

---
+++ app/tests/test_rsvp.py
@@ -0,0 +1,13 @@
+""" modules and libraries used for TestRsvp class"""
+import unittest
+from main.rsvp import Rsvp
+from main.person import Person
+
+
+class TestRsvp(unittest.TestCase):
+    """ Rsvp Test class """
+    def setUp(self):
+        self.beth = Rsvp("beth", "bethwambuimuniu@gmail.com",754234567556, 'guest')
+
+    def test_user_inherits_person(self):
+            self.assertTrue(issubclass(Rsvp, Person))
