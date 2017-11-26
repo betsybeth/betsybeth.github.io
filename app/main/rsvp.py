6ab80996ce2b02499d3ae9b0e178fa21eec7a9e1
Beth <bethwambuimuniu@gmail.com>
2017-11-27 00:10:18 +0300

    bug(flask-develop): Fix Rsvp  module

--- app/main/rsvp.py
+++ app/main/rsvp.py
@@ -6,7 +6,17 @@ from uuid import uuid4
 
 class Rsvp(Person):
     """ main class for the RSVP """
-    def __init__(self, phone_no, name, email, rsvp_category):
+    def __init__(self, name, email, phone_no, rsvp_category):
         super().__init__(name, email, rsvp_category)
         self.phone_no = phone_no
-        self.id = uuid4().hex
+        self._id = uuid4().hex
+
+    def rsvps_data(self):
+        """ returns the rsvp"""
+        return {
+            "_id" :self._id,
+             "name":self.name,
+             "email": self.email,
+             "phone_no":self.phone_no,
+             "rsvp_category":self.rsvp_category
+        }
